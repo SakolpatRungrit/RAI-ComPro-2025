e2b22aa54b82ff1dd8c38f4ff99eccc3ef1d9d99
SakolpatRungrit <ayasenakiri@gmail.com>
2025-08-25 13:02:57 +0000

    redo

--- ex01.c
+++ ex01.c
@@ -1,8 +1,32 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello world!");
-    return 0;
+    int i,j,rows;
+    printf("input you number:");
+    scanf("%d",&rows);
+    
+    for(i=1;i<=rows;i++)
+    {
+        printf("\n");
+        for(j=1;j<=(rows-i);j++){
+            printf(" ");            
+
+        }for(j=rows;j>rows-i;j--){
+            printf("%d",j);
+
+
+        }for(j=rows;j<=rows-i;j--){
+            printf("%d",j);
+
+        }for(j=rows-i+2;j<=rows;j++){
+            printf("%d",j);
+        }
+
+
+
+
+    }
+    
 
 
 
